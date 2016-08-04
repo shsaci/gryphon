d2e3d2daf4dc9f2dec808e12e989ed082b96bac2
Siobhan O'Connor <Siobhy@MacBook-Pro-3.local>
2016-08-05 11:47:57 +1200

    fixed merge

--- seeds/genres.js
+++ seeds/genres.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('genres').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('genres').insert({id: 1, genre: 'Science Fiction', genre_id:1 }),
+        knex('genres').insert({id: 2, genre: 'Poetry', genre_id:2 }),
+        knex('genres').insert({id: 3, genre: 'Classics', genre_id:3 }),
+        knex('genres').insert({id: 4, genre: 'Philosophy', genre_id:4 }),
+        knex('genres').insert({id: 5, genre: 'Graphic Novels', genre_id:5 })
       ]);
     });
 };
